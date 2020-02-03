db57f504f31f370499b848d709cfa916d67eb60e
Tamara Bonyai <tamamara18@gmail.com>
2020-02-03 12:17:07 +0100

    Counting with StateHook 1.0

---
+++ src/components/Counter.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  // Declares a new state variable, which we will call "count"
+  // Declares a setCount function that allows you to modify the count value
+  let [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <input
+        type="number"
+        value={count}
+        onChange={e => setCount(e.target.value)} 
+      />
+      <p>The counter is at : {count} </p>
+      <button onClick={() => setCount(count + 1)}>
+        +1
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        -1
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
